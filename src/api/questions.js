2b9ad0893aafaed63cb65812f070d75b426bb8f5
DavidKe91 <davidgkennedy@hotmail.com>
2019-01-22 22:00:57 +0000

    Committing React Quiz

---
+++ src/api/questions.js
@@ -0,0 +1,79 @@
+var quizQuestions = [
+  {
+    question: "What is the capital of Costa Rica?",
+    answerindex: 3,
+    answers: [
+      {
+        type: "city",
+        content: "Nicosia",
+        answer: false
+      },
+      {
+        type: "city",
+        content: "Santiago",
+        answer: false
+      },
+      {
+        type: "city",
+        content: "San Jose",
+        answer: false
+      },
+      {
+        type: "city",
+        content: "Bogota",
+        answer: false
+      }
+    ]
+  },
+  {
+    question:
+      "What luxury car company introduced the Boxster roadster in 1996?",
+    answerindex: 1,
+    answers: [
+      {
+        type: "car",
+        content: "Porsche",
+        answer: false
+      },
+      {
+        type: "car",
+        content: "Dodge",
+        answer: false
+      },
+      {
+        type: "car",
+        content: "Mitsubishi",
+        answer: false
+      }
+    ]
+  },
+  {
+    question:
+      "Located in southeast Vietnam, what is the former name of Ho Chi Minh City?",
+    answerindex: 4,
+    answers: [
+      {
+        type: "vietnam",
+        content: "Hue",
+        answer: false
+      },
+      {
+        type: "vietnam",
+        content: "Hanoi",
+        answer: false
+      },
+      {
+        type: "vietnam",
+        content: "Hoi An",
+        answer: false
+      },
+      {
+        type: "vietnam",
+        content: "Saigon",
+        answer: false
+      }
+    ]
+  }
+];
+
+export default quizQuestions;
---
+++ src/components/QuestionCount.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function QuestionCount(props) {
+  return (
+    <div className="questionCount">
+      Question <span>{props.counter}</span> of <span>{props.total}</span>
+      {props.counter === 3 ? (
+        <a className="result-link" href="" onClick={props.viewreults}>
+          View Results
+        </a>
+      ) : (
+        <div />
+      )}
+    </div>
+  );
+}
+
+QuestionCount.propTypes = {
+  counter: React.PropTypes.number.isRequired,
+  total: React.PropTypes.number.isRequired
+};
+
+export default QuestionCount;
